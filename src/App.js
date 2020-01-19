326f2695a4eeffce56449e3dcd22ec4845c5173d
akil-ahmed3 <akilahmed096@gmail.com>
2020-01-19 14:25:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import BookList from './components/BookList';
+import Navbar from './components/Navbar';
+import ThemeContextProvider from './context/ThemeContext';
+
+function App() {
+  return (
+    <div className="App">
+      <ThemeContextProvider>
+
+        <Navbar />
+        <BookList />
+      </ThemeContextProvider>
+    </div>
+  );
+}
+
+export default App;
